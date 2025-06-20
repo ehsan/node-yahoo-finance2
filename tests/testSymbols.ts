ebcc2342d8c9ca6e2925593c88612d3919ce5d2a
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2025-06-20 11:36:02 -0400

    Update testSymbols.ts

--- tests/testSymbols.ts
+++ tests/testSymbols.ts
@@ -27,6 +27,7 @@ const commonSymbols = [
   "GC=F", // Futures (#449),
   // "APS.AX", // .AX (#461); chart, historical, insights, recommendations, quoteSummary // Delisted
   "SWVXX", // Money Market (#905)
+  "ACO.X.TO", // TSX
 ];
 
 interface testSymbolsOptions {
